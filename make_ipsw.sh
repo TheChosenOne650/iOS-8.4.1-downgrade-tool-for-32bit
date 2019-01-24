3062ec5afe116856dc04be522784cee42bd2a455
NaluTech <hsell25@gmail.com>
2019-01-23 18:36:53 -0800

    iPhone4,1 and iPad2,5 support also bug fixes

--- make_ipsw.sh
+++ make_ipsw.sh
@@ -1,5 +1,37 @@
+#!/bin/bash
 echo Make sure you have an iOS 8.4.1 ipsw downloaded and placed in this directory before continuing
 sleep 1
-echo building ipsw
-./Tools/ipsw iPad2,1_8.4.1_12H321_Restore.ipsw iPad2,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/K93BM.plist
-echo ipsw creation complete
+echo chose a device
+
+## set device ##
+PS3='Please enter your choice: '
+options=("iPad 2 WiFi" "iPhone 4S" "iPad Mini WiFi" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "iPad 2 WiFi")
+         	echo building ipsw for iPad2,1 please wait...
+			./Tools/ipsw iPad2,1_8.4.1_12H321_Restore.ipsw iPad2,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/K93AP.plist
+			echo ipsw creation complete
+			break
+            ;;
+        "iPhone 4S")
+            echo building ipsw for iPhone4,1 please wait...
+			./Tools/ipsw iPhone4,1_8.4.1_12H321_Restore.ipsw iPhone4,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/N94AP.plist
+			echo ipsw creation complete
+			break
+            ;;
+        "iPad Mini WiFi")
+			echo building ipsw for iPad2,5 please wait...
+			./Tools/ipsw iPad2,5_8.4.1_12H321_Restore.ipsw iPad2,5_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/P105AP.plist
+			echo ipsw creation complete
+			break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+		
+echo Done!
--- restore.sh
+++ restore.sh
@@ -1,5 +1,35 @@
 echo Make sure your device is in kDFU mode
 echo you can do this on most devices using an app in Cydia repo: repo.tihmstar.net
 sleep 3
-./tools/idevicerestore -d -w iPad2,1_8.4.1_12H321_Odysseus.ipsw
+## set device ##
+PS3='Please enter your choice: '
+options=("iPad 2 WiFi" "iPhone 4S" "iPad Mini WiFi" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "iPad 2 WiFi")
+         	echo Restoring iPad2,1 please wait...
+			./tools/idevicerestore -e -o Tools/Manifests/K93AP.plist iPad2,1_8.4.1_12H321_Odysseus.ipsw 
+			break
+            ;;
+        "iPhone 4S")
+            echo Restoring iPhone4,1 please wait...
+			./tools/idevicerestore -e -o Tools/Manifests/N94AP.plist iPhone4,1_8.4.1_12H321_Odysseus.ipsw 
+			echo ipsw creation complete
+			break
+            ;;
+        "iPad Mini WiFi")
+            echo Restoring iPad2,5 please wait...
+			./tools/idevicerestore -e -o Tools/Manifests/P105AP.plist iPad2,5_8.4.1_12H321_Odysseus.ipsw 
+			echo ipsw creation complete
+			break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+		
+echo Done!
 echo restore complete
